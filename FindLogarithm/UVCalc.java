47180b8fbfb9cce152780eb1f13d75fcfaff3aa3
Nikita Maralov <blacknikboard@gmail.com>
2020-05-01 18:48:11 +0300

    Fixed invertions in modulo

--- FindLogarithm/UVCalc.java
+++ FindLogarithm/UVCalc.java
@@ -33,15 +33,15 @@ public class UVCalc {
         double z = (double) (zList.get(i - 1));
         FindLogarithm.print("Calculating u_" + i + ": ", false);
         if (z > l && z < ml) {
-            //FindLogarithm.print("0 < z0 < 1/3p; ", false);
+            // FindLogarithm.print("0 < z0 < 1/3p; ", false);
             result = UV.get(i - 1).getU() + 1;
             FindLogarithm.print("u_" + i + " = " + "u_(" + i + "-1) + 1 = " + result, true);
         } else if (z > ml && z < mh) {
-            //FindLogarithm.print("1/3p < z0 < 2/3p; ", false);
+            // FindLogarithm.print("1/3p < z0 < 2/3p; ", false);
             result = UV.get(i - 1).getU() * 2;
             FindLogarithm.print("u_" + i + " = " + "u_(" + i + "-1) * 2 = " + result, true);
         } else if (z > mh && z < h) {
-            //FindLogarithm.print("2/3p < z0 < p; ", false);
+            // FindLogarithm.print("2/3p < z0 < p; ", false);
             result = UV.get(i - 1).getU();
             FindLogarithm.print("u_" + i + " = " + "u_(" + i + "-1) = " + result, true);
 
@@ -128,6 +128,30 @@ public class UVCalc {
         return gcd(b, x);
     }
 
+    public static int invert(int n, int module) {
+        double result = n;
+        if (gcd(n, module) != 1) {
+            throw new RuntimeException(n + " not invertible in modulo " + module);
+        }
+        int i = 0;
+        while ((result % 1) != 0) {
+            if (i == module)
+                throw new RuntimeException("Error in calculations");
+            result = (1 + module) / n % module;
+            i++;
+        }
+        //FindLogarithm.print("invert"+result, true);
+        return (int) result;
+    }
+
+    public static int moduleAbs(int n, int module) {
+        while (n < 0) {
+            n += module;
+        }
+        //FindLogarithm.print("moduleABS"+n, true);
+        return n;
+    }
+
     public int solve() {
         int xZero = 0;
         int i, up, down, j = -1;
@@ -146,9 +170,7 @@ public class UVCalc {
         // + b.pow(UV.get(i).getV()).mod(BigInteger.valueOf(module)) + " = "
         // + a.pow(UV.get(j).getU()).mod(BigInteger.valueOf(module)) + " * "
         // + b.pow(UV.get(j).getV()).mod(BigInteger.valueOf(module)), true);
-        xZero = (up / down) % (module - 1);
-        while (xZero < 0)
-            xZero += (module - 1);
+        xZero = (moduleAbs(up, module - 1) * invert(moduleAbs(down, module - 1), module - 1)) % (module - 1);
         FindLogarithm.print("z_" + i + " = " + "z_" + j, true);
         FindLogarithm.print("x = (" + UV.get(i).getU() + " - " + UV.get(j).getU() + ") / (" + UV.get(j).getV() + " - "
                 + UV.get(i).getV() + ") = " + xZero + " mod(" + (module - 1) + ")", true);
